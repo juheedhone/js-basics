4eea25b740913a2499d9aa713f2a0e2a65dc8ec5
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-10-20 23:29:37 +0530

    operators

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-let x = 10;
-let y = 20;
+let x = 1;
 
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y);
-// console.log(x ** y);
+//relational operators
+ console.log(x > 1);
+ console.log(x < 1);
+ console.log(x <= 1);
+ console.log(x >= 1);
 
-//increment
-//console.log(++x);
-// console.log(x);
+// //equality operators
 
-// //decrement
-// console.log(--x);
-// console.log(x);
+ console.log(x === 1);
+console.log(x !== 1);
+
+// //equality operators (strict equality(type + value))
+ console.log(1 === 1);
+ console.log('1' === 1);
+
+//equality operators (loose equality(value))
+ console.log(1 == 1);
+ console.log('1' == 1);
+console.log(true == 1);
+
+//ternarry operator or conditional operator
+
+//if a cosutomer has more than 100 points,
+//they are a 'gold' customer, otherwise,
+//they are a 'silver' customer.
+
+let points = 10;
+let type = points < 11 ? "gold " : "silver";
+console.log(type);
