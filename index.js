d52ca0655655642d88f21360db543b574583b45a
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-10-25 23:42:15 +0530

    break and continue

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// for (let i = 1; i <= 5; i++) {
-//   console.log("Hello World", i);
-// }
+let i = 0;
+while (i <= 100) {
+  // if (i === 50) break;
 
-// for-in loop
-
-const person = {
-  name: "John",
-  age: 30,
-};
-
-for (let key in person) {
-  console.log(key, person[key]);
+  if (i % 2 === 0) {
+    i++;
+    continue;
+  }
+  console.log(i);
+  i++;
 }
-
-// for-of loop
-const colors = ["red", "green", "blue"];
-
-for (let color of colors) 
-  console.log(colors);
