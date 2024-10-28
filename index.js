6a0e30fb42799fb6cb181a586b5a57210cd3d938
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-10-28 17:40:59 +0530

    landscape exercise2

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-function maximum(num1, num2) {
-  if (num1 > num2) console.log(num1);
-  else console.log(num2);
-}
+// function maximum(num1, num2) {
+//   if (num1 > num2) console.log(num1);
+//   else console.log(num2);
+// }
 
-function minimum(num1, num2) {
-  if (num1 > num2) console.log(num2);
-  else console.log(num1);
-}
+// function minimum(num1, num2) {
+//   if (num1 > num2) console.log(num2);
+//   else console.log(num1);
+// }
 
-maximum(5, 7);
-minimum(5, 7);
+// maximum(5, 7);
+// minimum(5, 7);
+
+console.log(isLandscape(300, 600));
+function isLandscape(width, height) {
+  return width > height;.
+}
