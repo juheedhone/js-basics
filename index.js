23730646bfb40aa9787395c8554773f5e82298de
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-10-30 00:48:39 +0530

    showNumbers exercise

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-//
+showNumbers(50);
 
-// speed limit = 70
-// 5 = 1 point
-// math.floor(1.3)
-// 12 points-> suspended
-
-const output = checkSpeed(112);
-console.log(output);
-
-function checkSpeed(speed) {
-  if (speed <= 70) return "ok";
-
-  let extraSpeed = speed - 70;
-  let points = Math.floor(extraSpeed / 5);
-
-  if (points > 12) return "suspended";
-  else return points + "points";
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    if (i % 2 === 0) console.log("even" + " : ", i);
+    else console.log("odd " + " : ", i);
+  }
 }
+
+// console.log(showNum(4));
+
+// function showNum(num) {
+//   if (num % 2 === 0) return num + " : " + "even";
+//   else return "odd";
+// }
