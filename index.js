22b60698fd81237984ae001389c34ffe6f05e84d
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-04 23:20:30 +0530

    star exercise

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-const marks = [80, 100, 100, 80];
+showStars(50);
 
-// average=80
-
-// 1-59:F
-// 60-69:D
-// 70-79:C
-// 80-89:B
-// 90-100:A
-
-function calculateGrade(marks) {
-  let total = 0;
-  for (let key of marks) total = key + total;
-
-  const average = total / marks.length;
-
-  if (average < 59) return "F";
-  else if (average < 69) return "D";
-  else if (average < 79) return "C";
-  else if (average < 89) return "B";
-  else return "A";
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++) {
+    let pattern = "";
+    for (let i = 0; i < row; i++) 
+        pattern += "*";
+    console.log(pattern);
+  }
 }
-
-console.log(calculateGrade(marks));
