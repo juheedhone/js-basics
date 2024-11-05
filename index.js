da308b36968e28d06f10091199945e50264064ce
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-05 22:21:54 +0530

    constructor notation

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-function createCircle(radius) {
-  return {
-    radius,
+// constructor notation
 
-    draw() {
-      console.log("draw");
-    },
+function Circle(radius) {
+  this.radius = radius;
+  this.radius = function () {
+    console.log("draw");
   };
 }
 
-const circle1 = createCircle(2);
-console.log(circle1);
-
-const circle2 = createCircle(4);
-console.log(circle2);
-
-//method
+const circle = new Circle(1);
