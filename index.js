0f9c6883e43f5e2aeca0ea400d401b3b2444e81a
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-08 14:52:08 +0530

    Date object

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// boolean true,false
-// object {}
-// string '',""
-// template ``
+const now = new Date();
+const date1 = new Date("may 11 2024 09:00");
+const date2 = new Date(2024, 4, 11, 9);
 
-const another = `this is 
-my 'first' message`;
+now.setFullYear(2024);
+
+// now.toDateString()
+// 'Fri Nov 08 2024'
+
+// now.toTimeString()
+// '14:49:17 GMT+0530 (India Standard Time)'
+
+// now.toISOString()
+// '2024-11-08T09:19:17.778Z'
