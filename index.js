5b2381917d03bb4073df81d9a05a0438a3591a39
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-11 01:44:19 +0530

    combining and slicing

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-let numbers = [1, 2, 3, 4];
-let another = numbers;
+const first = [{ id: 1 }];
+const second = [5, 6, 7, 8];
 
-// solution 1
-numbers = [];
+const combined = first.concat(second);
 
-// solution 2
-numbers.length = 0;
+first[0].id = 4;
+const slice = combined.slice(2, 4);
 
-// solution 3
-numbers.splice(0, numbers.length);
-
-// solution 4
-while (numbers.length > 0) numbers.pop();
-
-console.log(numbers);
-console.log(another);
+console.log(slice);
+console.log(combined);
