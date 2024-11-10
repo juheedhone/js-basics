5c57c61035f485ecc9ec4b5f63ea95d3576e53f9
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-11 01:52:20 +0530

    spread Operator

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const first = [{ id: 1 }];
+const first = [1, 2, 3, 4];
 const second = [5, 6, 7, 8];
 
-const combined = first.concat(second);
+// const combined = first.concat(second);
+const combined = [...first, "a", "b", ...second];
 
-first[0].id = 4;
-const slice = combined.slice(2, 4);
+// const slice = combined.slice(2, 4);
+const copy = [...combined];
 
-console.log(slice);
 console.log(combined);
