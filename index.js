6be30a60fe4de271856bf97d005293157f67b6a0
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-10 19:17:05 +0530

    exercise constructor

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-let address1 = new Address("a", "b", "c");
-let add2 = new Address("a", "b", "c");
+// title
+// body
+// author
+// views
+// comment
+//  (author , body )
+// isLive
 
-console.log(areEqual(address1, add2));
-console.log(areSame(address1, add2));
-
-// factory function
-
-function Address(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
+function Post(title, body, author) {
+  (this.title = title),
+    (this.body = body),
+    (this.author = author),
+    (this.views = 0),
+    (this.comment = {
+      author: "d",
+      body: "e",
+    }),
+    (this.isLive = isLive);
 }
 
-function areEqual(address1, add2) {
-  return (
-    address1.street === add2.street &&
-    address1.city === add2.city &&
-    address1.zipCode === add2.zipCode
-  );
-}
-
-function areSame(address1, add2) {
-  return address1 === add2;
-}
+const object1 = new Post("a", "b", "c");
+console.log(object1);
