9f96a582f4fcd3c7e2e264d363f9baf4d34db5ea
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-11 17:31:02 +0530

    map method

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const numbers = [1, -1, 2, 3, 4];
 
-// filter
+// mapping html
+// const filtered = numbers.filter((n) => n >= 0);
+
+// const items = filtered.map((n) => "<li>" + n + "</li>");
+
+// const html = '<ul>' + items.join('') + '</ul>' ;
+
+// console.log(html);
+
+// mapping object
+
 const filtered = numbers.filter((n) => n >= 0);
 
-console.log(filtered);
+const items = filtered.map((n) => ({value: n}));
+
+// const items = numbers.filter((n) => n >= 0).map((n) => ({ value: n }));
+
+console.log(items);
