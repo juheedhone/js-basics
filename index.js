9ad1ef79f560c4739f34944dbdaf37d4a24dc986
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-11 17:51:21 +0530

    reducing array

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+// reducing array
+
 const numbers = [1, -1, 2, 3, 4];
 
-// mapping html
-// const filtered = numbers.filter((n) => n >= 0);
+// let sum = 0;
+// for (let n of numbers) sum += n;
 
-// const items = filtered.map((n) => "<li>" + n + "</li>");
 
-// const html = '<ul>' + items.join('') + '</ul>' ;
+// a= 0 , c= 1 => a=1
+// a= 1, c= -1 => a=0
+// a= 0 , c= 2 => a=2
+// a= 2 , c= 3 => a=5
 
-// console.log(html);
 
-// mapping object
+// a= 1 , c= -1 => a=0
+// a= 0, c= 2 => a=2
+// a= 2, c= 3 => a=5
 
-const filtered = numbers.filter((n) => n >= 0);
 
-const items = filtered.map((n) => ({value: n}));
+const sum = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
 
-// const items = numbers.filter((n) => n >= 0).map((n) => ({ value: n }));
-
-console.log(items);
+console.log(sum);
