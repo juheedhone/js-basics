7b1a8fbf565616529c99320937bc0a8df5a29654
shrikantkalar023 <shrikantkalar023@gmail.com>
2024-11-12 18:40:42 +0530

    exercise 4

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+// exercise 4
+const numbers = [1, 2, 3, 4];
 
-// exercise 3
-const number = [1, 2, 3, 4];
+console.log(move(numbers, 3, -2));
 
-const output = except(number, [1]);
-console.log(output);
+function move(array, index, offset) {
+  let output = [];
+  const elementToMove = array[index];
+  const elementNewIndex = index + offset;
 
-function except(array, excluded) {
-  const output = [];
-  for (let element of array)
-    if (!excluded.includes(element)) output.push(element);
+  if (elementNewIndex >= array.length && elementNewIndex < 0)
+    console.error("bad offset");
+
+  if ((offset = 0)) return array;
+
+  output =
+    offset > 0
+      ? [
+          ...array.slice(0, index),
+          ...array.slice(index + 1, elementNewIndex + 1),
+          elementToMove,
+          ...array.slice(elementNewIndex + 1),
+        ]
+      : [
+          ...array.slice(0, elementNewIndex),
+          elementToMove,
+          ...array.slice(elementNewIndex, index),
+          ...array.slice(index + 1),
+        ];
   return output;
 }
