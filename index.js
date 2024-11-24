6ff9097fd762b6db44b00ae772d39ce3b720bd65
shrikantkalar023 <juheedhone12@gmail.com>
2024-11-25 02:23:29 +0530

    exercise palindrome

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// Write a function that takes two numbers (start, end) and returns the sum of all numbers between them, inclusive.
+// Write a program to check if a given string is a palindrome (reads the same backward).
 
-
-function adding(start, end) {
-  let output = 0;
-  for (let i = start; i <= end; i++) {
-    output = output + i;
+function palindrome(word) {
+  for (let i = 0; i < Math.floor(word.length / 2); i++) {
+    if (word[i] !== word[word.length - 1 - i]) return false;
   }
-  return output;
+  return true;
 }
 
-console.log(adding(1, 100));
+console.log(palindrome("radar"));
