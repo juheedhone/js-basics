e3f77a4c4f2966af802ffb33075672df83317b8c
shrikantkalar023 <juheedhone12@gmail.com>
2024-11-25 00:44:22 +0530

    exercise range addition

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const numbers = [11, 1, 2, 3, 4, 8, 9];
-
-const max = getMax([1, 2, 11]);
-
-function getMax(array) {
-  if (array.length === 0) return undefined;
-
-//   solution 1
-    if ((maximum = Math.max(...array))) {
-      return maximum;
-    }
+// Write a function that takes two numbers (start, end) and returns the sum of all numbers between them, inclusive.
 
 
-//   solution 2
-    let max = array[0];
-
-    for (let i = 1; i < array.length; i++) {
-      if (array[i] > max) max = array[i];
-    }
-    return max;
-
-
-//   solution 3(reduced method)
-
-  return max = array.reduce((a, b) => (a > b )? a : b);
-  
+function adding(start, end) {
+  let output = 0;
+  for (let i = start; i <= end; i++) {
+    output = output + i;
+  }
+  return output;
 }
 
-console.log(max);
+console.log(adding(1, 100));
