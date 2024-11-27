b43a580608954c3dd82db04a3939b9be77e979d7
shrikantkalar023 <juheedhone12@gmail.com>
2024-11-28 00:22:21 +0530

    get set in function

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// movie
-const movies = [
-  { title: "a", year: 2018, rating: 4.5 },
-  { title: "b", year: 2018, rating: 4.7 },
-  { title: "c", year: 2018, rating: 3 },
-  { title: "d", year: 2017, rating: 4.5 },
-];
 
-const movie = movies
-  .filter((m) => m.year === 2018 && m.rating >= 4)
-  .sort((a, b) => a.rating - b.rating)
-  .reverse()
-  .map((m) => m.title);
+// setter and getter 
 
-console.log(movie);
+const person = {
+  firstName: "mosh",
+  lastName: "Hamedani",
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+
+  set fullName(value) {
+    const parts = value.split(" ");
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+
+
+person.fullName = "john smith";
+console.log(person);
