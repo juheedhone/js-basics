55da91e68846cd8e68268a5eb123ad6589b647b5
shrikantkalar023 <juheedhone12@gmail.com>
2024-11-29 00:58:26 +0530

    exercise 3(error handling)

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// countOccurrence
-
-const number = [1, 2, 3, 4, 5, 2];
-
-const count = countOccurrence(number, 2);
-
-console.log(count);
+// countOccurrence handling error
 
 function countOccurrence(array, searchElement) {
-  // let count = 0;
-  // for (let element of array) {
-  //   if (searchElement === element) count++;
-  // }
-  // return count;
-
+  if (!Array.isArray(array)) {
+    throw new Error("please give array");
+  }
   return array.reduce((accumulator, current) => {
     const occurrence = current === searchElement ? 1 : 0;
     console.log(accumulator, searchElement, current);
     return accumulator + occurrence;
   }, 0);
+}
+
+const number = [1, 2, 3, 4, 5, 2];
+
+try {
+  const count = countOccurrence(null, 2);
+  console.log(count);
+} catch (e) {
+  console.error(e.message);
 }
