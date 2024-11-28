af70b89ec7f38d34b37d25f628ad997b4e947056
shrikantkalar023 <juheedhone12@gmail.com>
2024-11-28 19:37:13 +0530

    exercise 2(area of circle)

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-// SUM OF ARGUMENTS
+// Area of Circle
 
-// solution1
+const circle = {
+  radius: 2,
+  get area() {
+    return 3.14 * this.radius * this.radius;
+  },
+};
 
-// function sum(...arguments) {
-//   // console.log(arguments);
-//   // [[1, 2, 3, 4],[]];
-//   return arguments.reduce((a, b) => {
-//     if (Array.isArray(b)) {
-//       return a + b.reduce((e1, e2) => e1 + e2);
-//     }
-//     return a + b;
-//   }, 0);
-// }
-// console.log(sum([1, 2, 3, 4], [1, 2, 3, 4, 5]));
-
-
-// solution 2
-
-// function sum(...arguments) {
-//   // console.log(arguments);
-//   // [[1, 2, 3, 4],[4]];
-
-//   return arguments[0].reduce((a, b) => a + b);
-// }
-// console.log(sum([1, 2, 3, 4, 5],[4]));
-
-// solution 3
-
-function sum(...items) {
-  // console.log(arguments);
-  // [[1, 2, 3, 4],[4]];
-
-  if (items.length === 1 && Array.isArray(items[0])) {
-    items = [...items[0]];
-  }
-
-  return items.reduce((a, b) => a + b);
-}
-console.log(sum(1, 2, 3, 4, 5));
+// circle.area = 20;
+console.log(circle.area);
