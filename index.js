1c5802b161947b02a266463f93ecb58579958c63
shrikantkalar023 <juheedhone12@gmail.com>
2024-11-28 15:45:17 +0530

    try and catch

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-
-// setter and getter 
+// try and catch
 
 const person = {
   firstName: "mosh",
   lastName: "Hamedani",
-  get fullName() {
-    return `${person.firstName} ${person.lastName}`;
-  },
 
   set fullName(value) {
+    if (typeof value !== "string") throw new Error("value is not string");
     const parts = value.split(" ");
+    if (parts.length !== 2) throw new Error("enter a first and last name");
     this.firstName = parts[0];
     this.lastName = parts[1];
   },
 };
 
+try {
+  person.fullName = "";
+} catch (e) {
+  alert(e);
+}
 
-person.fullName = "john smith";
 console.log(person);
