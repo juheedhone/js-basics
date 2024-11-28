8fa6f32c2a0e93a515fb7d3fc9913b97c00c8b52
shrikantkalar023 <juheedhone12@gmail.com>
2024-11-28 19:52:33 +0530

    exercise 5(array)

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-// Area of Circle
+// countOccurrence
 
-const circle = {
-  radius: 2,
-  get area() {
-    return 3.14 * this.radius * this.radius;
-  },
-};
+const number = [1, 2, 3, 4, 5, 2];
 
-// circle.area = 20;
-console.log(circle.area);
+const count = countOccurrence(number, 2);
+
+console.log(count);
+
+function countOccurrence(array, searchElement) {
+  // let count = 0;
+  // for (let element of array) {
+  //   if (searchElement === element) count++;
+  // }
+  // return count;
+
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    console.log(accumulator, searchElement, current);
+    return accumulator + occurrence;
+  }, 0);
+}
